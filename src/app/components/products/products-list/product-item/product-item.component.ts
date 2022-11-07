1094c36c8e80d80198a9e4d583f5ac8838f7a7c4
alafasy <bakrim.issam@gmail.com>
2022-11-07 10:49:21 +0100

    [1]decomposition of component

---
+++ src/app/components/products/products-list/product-item/product-item.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/model/product.model';
+import { ActionEvent, AppDataState, ProductActionsTypes } from 'src/app/state/product.state';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+
+  @Input() product!:Product;
+  @Output() eventEmitter:EventEmitter<ActionEvent> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(p:Product){
+    this.eventEmitter.emit({type:ProductActionsTypes.SELECT_PRODUCT,payload:p})
+  }
+
+  
+  onDelete(p:Product){
+   this.eventEmitter.emit({type:ProductActionsTypes.DELETE_PRODUCT,payload:p})
+   }
+
+   onEdit(p:Product){
+     this.eventEmitter.emit({type:ProductActionsTypes.EDIT_PRODUCT,payload:p})
+   }
+
+}
